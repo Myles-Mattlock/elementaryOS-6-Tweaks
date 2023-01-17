923b9601915251a30d17c8e86e784542b6e68035
Myles-Mattlock <65444145+Myles-Mattlock@users.noreply.github.com>
2023-01-17 22:51:20 +0000

    Added initial files

---
+++ README.md
@@ -0,0 +1,15 @@
+# mint-xfce-setup
+Linux Mint customizations from Myles Mattlock
+ 
+## Requirements
+Linux mint 20.2 or higher (not tested with older versions)
+
+Or Linix mint to have xfce installed
+### Base Stuff - Root
+
+_Run as shown below_
+```
+git clone https://github.com/Myles-Mattlock/mint-xfce-setup
+cd mint-xfce-setup/
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Copying and moving files to the right locations 
+#sudo cp plank.desktop /etc/xdg/autostart
+# Remove obslete apt packages
+sudo apt remove atril* parole* xarchiver* xsane* xterm* xfburn* exfalso* catfish* gnome-disk-utility* gnome-logs* gucharmap* seahorse* onboard* xfce4-whiskermenu-plugin xfce4-xkb-plugin drawing pix* hexchat* thunderbird* celluloid* libreoffice* -y
+
+sudo apt purge atril* parole* xarchiver* xsane* xterm* xfburn* exfalso* catfish* gnome-disk-utility* gnome-logs* gucharmap* seahorse* onboard* xfce4-whiskermenu-plugin xfce4-xkb-plugin drawing pix* hexchat* thunderbird* celluloid* libreoffice* -y
+
+sudo apt autoremove -y
+
+#Remove obslete flatpak packages
+
+
+
+# Update packages list
+sudo apt update
+
+sudo apt upgrade -y
+
+# Add base packages
+sudo apt install unzip evince neofetch plank htop vlc libdvd-pkg rhythmbox kodi pulseeffects pavucontrol fonts-noto-color-emoji fonts-firacode fonts-font-awesome gdebi -y
+
+# Setting up the dpkg
+sudo dpkg-reconfigure libdvd-pkg
+
+#Addition packages to install
+cd Packages/
+sudo gdebi code.deb -n
+sudo gdebi GitHubDesktop-linux.deb -n
+sudo gdebi google-chrome-stable_current.deb -n
+sudo gdebi teams.deb -n
+
+#Adding libreoffice-fresh
+cd LibreOffice_7.2.4.1_Linux_x86-64_deb/DEBS/
+sudo dpkg -i *.deb
+# Make Theme folders
+mkdir -p ~/.themes ~/.fonts
+
+# `ubuntu nerd font
+cd ..
+unzip UbuntuMono.zip -d ~/.fonts
+fc-cache -vf
+
+# Ms-fonts
+sudo apt install ttf-mscorefonts-installer -y
+
+#Finish off with a reboot
+echo "System will reboot in 10 seconds"
+sleep 10
+sudo reboot
